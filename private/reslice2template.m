1718b1cf120afa1e0968f1c5eb5092b284beae33
Mikael Brudfors <brudfors@gmail.com>
2020-04-20 11:21:23 +0100

    Refactoring

--- private/reslice2template.m
+++ private/reslice2template.m
@@ -1,21 +1,38 @@
 function [Nii,M] = reslice2template(Nii,M,pth_template)
 fprintf('Reslicing to template...')
 Vr           = spm_vol(pth_template);
+Vr           = Vr(1);
 flags        = struct;
 flags.params = [0 0 0 0 0 0 1 1 1 0 0 0];
 N            = numel(Nii{1});
 R            = cell(1,N);
+
+Mr  = Vr.mat;
+dmr = Vr.dim;
+vxr = sqrt(sum(Mr(1:3,1:3).^2));
+
+vx   = [1 1 1];
+samp = vxr./vx;
+D    = diag([samp 1]);
+Mr   = Mr/D;
+dmr  = floor(D(1:3,1:3)*dmr')';
+
 for n=1:N
     f  = Nii{1}(n).dat.fname;
     Vn = spm_vol(f);    
-    x  = spm_coreg(Vr,Vn,flags);
     
-    Rn   = spm_matrix(x(:)');
-    Mn   = Vn.mat\Rn*Vr.mat;
+    if 0
+        x  = spm_coreg(Vr,Vn,flags);    
+        Rn = spm_matrix(x(:)');
+    else
+        Rn = eye(4);
+    end
+    
+    Mn   = Vn.mat\Rn*Mr;
     R{n} = Rn;
     M{n} = M{n}*R{n};
     
-    y = Affine(Vr.dim,Mn);
+    y = Affine(dmr,Mn);
     
     intrp = [4 4 4 0 0 0];
     im    = single(Nii{1}(n).dat());
@@ -28,15 +45,15 @@ for n=1:N
     
     [pth,nam,ext] = fileparts(f);
     npth          = fullfile(pth,['rt' nam ext]);
-    create_nii(npth,im,Vr.mat,'float32','resliced',...
+    create_nii(npth,im,Mr,'float32','resliced',...
                Nii{1}(n).dat.offset,Nii{1}(n).dat.scl_slope,Nii{1}(n).dat.scl_inter);    
     
     Nii{1}(n) = nifti(npth);
     delete(f);
     
-    if do_affinereg
+    if 0
         npth = fullfile(pth,['R_' nam '.mat']);
-        save(npth,'Rn');
+        save(npth,'Rn');    
     end
 end
 
