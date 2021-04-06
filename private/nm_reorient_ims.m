71eef860bfdffcafbb9f93321f30d1de257741cc
Paul Wright <32168177+pwrightkcl@users.noreply.github.com>
2021-04-06 16:07:58 +0100

    Keep labels in alignment

--- private/nm_reorient_ims.m
+++ private/nm_reorient_ims.m
@@ -9,5 +9,15 @@ for n=1:N
     f         = nm_reorient(f);
     Nii{1}(n) = nifti(f);
 end
+if numel(Nii) > 1
+    % Keep labels in alignment
+    for n=1:N
+        if n > numel(Nii{2}) || isempty(Nii{2}(n).dat), continue; end
+        
+        f         = Nii{2}(n).dat.fname;    
+        f         = nm_reorient(f,[],'',0);
+        Nii{2}(n) = nifti(f);
+    end    
+end
 fprintf('done!\n')
 %==========================================================================
